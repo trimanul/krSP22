8145a38dbcc0c63a5493a98a6d696ca2a075ef20
trimanul <ivanko150702@gmail.com>
2022-03-26 18:41:23 +0300

    small fix

--- Kursovaya/Source.cpp
+++ Kursovaya/Source.cpp
@@ -223,7 +223,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
         PostQuitMessage(0);
         return 0;
     }
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);;
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 
 }
 
