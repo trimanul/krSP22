d283d277c3b28192b91da36fe47bac57aed0a40a
trimanul <ivanko150702@gmail.com>
2022-03-27 15:20:31 +0300

    small changes

--- Kursovaya/resource.h
+++ Kursovaya/resource.h
@@ -13,13 +13,15 @@
 #define ID_FILE_SAVE                    40003
 #define ID_FILE_SAVEAS                  40004
 #define ID_FILE_EXIT                    40005
+#define ID_ABOUT                        40006
+#define ID_ABOUT_KRIUK5                 40007
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
-#define _APS_NEXT_COMMAND_VALUE         40006
+#define _APS_NEXT_COMMAND_VALUE         40008
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
